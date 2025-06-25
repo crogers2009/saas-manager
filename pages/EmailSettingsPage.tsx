e384b1838bcf2ef320d145d2741a2bce32774b75
crogers2009 <102686759+crogers2009@users.noreply.github.com>
2025-06-25 17:33:22 -0500

    Fix missing Select import in EmailSettingsPage

--- pages/EmailSettingsPage.tsx
+++ pages/EmailSettingsPage.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Button from '../components/Button';
+import Select from '../components/Select'; // Added missing import
 import LoadingSpinner from '../components/LoadingSpinner';
 import { SMTPConfig, NotificationPreference, NotificationType } from '../types';
 import { getSMTPConfig, updateSMTPConfig, testSMTPConnection, getUserNotificationPreferences, updateNotificationPreferences } from '../services/apiService';
